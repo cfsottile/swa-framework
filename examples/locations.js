9b08f6312fa2dc045f140373d98cf5e4cbe1343d
Cristian Sottile <cfsottile@gmail.com>
2017-03-08 17:34:30 -0300

    added two examples

---
+++ examples/locations.js
@@ -0,0 +1,48 @@
+var select = () => {
+    // in order to get a proper array, I have to do this
+    var elements = document.getElementById("filming_locations_content").children;
+    var arr = [].slice.call(elements);
+    arr.splice(0, 1);
+    return arr;
+};
+
+var extract = gExtract((location_container) => {
+    return [location_container.children[0].children[0].innerHTML.split(',')[0]];
+});
+
+var get = gGet(
+    [
+      "prefix dbpedia-owl: <http://dbpedia.org/ontology/>" + "\n" +
+        "prefix dbpprop: <http://dbpedia.org/property/>" + "\n" +
+        "select ?o where { ?s dbpprop:name \"",
+      "\"@en ." + "\n" + "?s dbpedia-owl:thumbnail ?o }"
+    ],
+    (data) => {
+        var results = {};
+        if (data.results.bindings.length > 0) {
+          results["thumbnail"] = data.results.bindings[0].o.value;
+        }
+        return results;
+    }
+);
+
+var build = gBuildNN1(
+  '<img id="itemTemplate" src={{thumbnail}}>',
+  '<marquee id="semantic_augmented_image_container" class="article">{{data}}</marquee>');
+
+var inject = gInject1(
+  () => {
+    return document.getElementById("main");
+  },
+  (node, built) => {
+    node.insertBefore(built, document.getElementById("see_also"));
+  }
+);
+
+augment(
+    select,
+    extract,
+    get,
+    build,
+    inject
+);
---
+++ examples/wikipedia.js
@@ -0,0 +1,41 @@
+var select = () => {
+    return [document.getElementById("firstHeading")];
+};
+
+var extract = gExtract((node) => {
+    return [node.innerHTML];
+});
+
+var get = gGet(
+    [
+        "select ?thatPage ?appearsAs ?ofWPPage where {\n" +
+        "?thatPage <http://xmlns.com/foaf/0.1/isPrimaryTopicOf> <http://en.wikipedia.org/wiki/", ">.\n" +
+        "?of <http://xmlns.com/foaf/0.1/isPrimaryTopicOf> ?ofWPPage.\n" +
+        "?of ?appearsAs ?thatPage.}"
+    ],
+    (data) => {
+        return data.results.bindings.map((result) => {
+            return {
+                "resource": result.ofWPPage.value,
+                "as": result.appearsAs.value
+            };
+        });
+    }
+);
+
+var build = gBuildNM1(
+    '<ul><li>{{resource}} - {{as}}</li></ul>',
+    '<h2><span class="mw-headline" id="Semantic-related">Semantic related</span>{{data}}',
+    '{{data}}');
+
+var inject = gInjectNto1(
+  () => { return document.evaluate("//*[@id=\"mw-content-text\"]/p[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; },
+  (node, built) => { node.appendChild(built); });
+
+augment(
+    select,
+    extract,
+    get,
+    build,
+    inject
+);
