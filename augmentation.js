e43cebaa5e1cb539f4ccc047df2863c817852de7
Cristian Sottile <cfsottile@gmail.com>
2017-03-08 17:33:00 -0300

    building process redesigned

--- augmentation.js
+++ augmentation.js
@@ -1,11 +1,23 @@
-function augment(select, extract, get, build, inject) {
-    inject(build(get(extract(select()))));
+import { gBuildNNN, gBuildNN1, gBuildNMN, gBuildNM1 } from './build'
+
+function augment(select, extract, fetch, build, inject) {
+    inject(build(fetch(extract(select()))));
 }
 
-function gGet(baseQuery, parser) {
+// extraction functions
+
+function gExtract(parser) {
+    return function (selectedHTMLNodes) {
+        return selectedHTMLNodes.map(parser);
+    };
+}
+
+// fetching functions
+
+function gFetch(baseQuery, parser) {
     return function(extractedElements) {
-        return extractedElements.map(query(baseQuery));
-    }
+        return extractedElements.map(query(baseQuery)).map(parser);
+    };
 }
 
 function query(base) {
@@ -14,99 +26,104 @@ function query(base) {
         xhr.open('GET', buildURI(buildQuery(base, args)), false);
         xhr.send();
         return JSON.parse(xhr.responseText);
-    }
+    };
 }
 
 function buildURI(query) {
-    return encodeURI("http://dbpedia.org/sparql?query=" + query + "&format=json");
+    return encodeURI("https://dbpedia.org/sparql?query=" + query + "&format=json");
 }
 
 function buildQuery(base, args) {
     return buildQueryR("", base, args);
 }
 
-function buildQueryR(res, base, args) {
-    if (args.length > 0) {
+function buildQueryR(res, [carBase, ...cdrBase], [carArgs, ...cdrArgs]) {
+    if (cdrBase.length > 0) {
         return buildQueryR(
-            res.concat(base[0]).concat(args[0]),
-            base.slice(1),
-            args.slice(1));
+            res.concat(carBase).concat(carArgs),
+            cdrBase,
+            cdrArgs);
     } else {
-        return res.concat(base[0]);
+        return res.concat(carBase);
     }
 }
 
-function gBuildNtoN(htmlString) {
+// building functions
+
+function gBuildNNN(template) {
     return function(gottenDictionaries) {
-        return gottenDictionaries.map(fulfillItemTemplate(itemTemplateNtoN(htmlString)));
-    }
+        return gottenDictionaries.map(fulfillHtml(template));
+    };
 }
 
-function itemTemplateNtoN(htmlString) {
-    var htmlNode = (new DOMParser()).parseFromString(htmlString, "text/html");
-    var itemTemplateNode = htmlNode.getElementById("itemTemplate")
-    return itemTemplateNode.getElementsByTagName("body")[0];
-}
-
-function gBuildNto1(htmlString) {
+function gBuildNN1(template1,template2) {
     return function(gottenDictionaries) {
-        var finalHTMLNode = gottenDictionaries.reduce(
-            function(prev, curr) {
-                return addFulfilledItem(
-                    prev,
-                    fulfillItemTemplate(itemTemplateNto1(htmlString))(curr)
-                );
-            },
-            (new DOMParser()).parseFromString(htmlString, "text/html");
-        );
-        return removeItemTemplateFrom(finalHTMLNode).getElementsByTagName("body")[0].firstChild;
-    }
+        var htmls = gottenDictionaries.map(fulfillHtml(template1));
+        return fulfillHtml(template2)({ "data": nodeToString(fold(htmls)) });
+    };
 }
 
-function itemTemplateNto1(htmlString) {
-    var htmlNode = (new DOMParser()).parseFromString(htmlString, "text/html");
-    return htmlNode.getElementById("itemTemplate");
-}
-
-function addFulfilledItem(main, fulfilledItem) {
-    main.getElementById("itemTemplate").parentNode.appendChild(fulfilledItem);
-}
-
-function removeItemTemplateFrom(finalHTMLNode) {
-    finalHTMLNode.getElementById("itemTemplate").parentNode.removeChild(node);
-}
-
-function prepare(itemTemplate) {
-    var item = itemTemplate.cloneNode(true);
-    item.setAttribute("id", "item");
-    return nodeToString(item);
-}
-
-function generateHTMLNode(itemStr) {
-    return ((new DOMParser()).parseFromString(itemStr, "text/html")).getElementById("item");
-}
-
-function fulfillItemTemplate(itemTemplate) {
-    return function(data) {
-        var itemStr = prepare(itemTemplate);
-        var toFulfill = itemStr.match(/{{(.*?)}}/g);
-        toFulfill.forEach(function (e, i, a) {
-            itemStr = itemStr.replace(e, data[e.slice(2, e.length - 2)]);
+function gBuildNMN(template1,template2) {
+    return function(gottenDictionariess) {
+        var htmlss = gottenDictionariess.map(
+            (gottenDictionaries) => { return gottenDictionaries.map( fulfillHtml(template1) ); });
+        return htmlss.map((htmls) => {
+            return fulfillHtml(template2)({ "data": nodeToString(fold(htmls)) });
         });
-        return generateHTMLNode(itemStr);
-    }
+    };
 }
 
-function gInjectNtoN(nodeGetter, injection) {
+function gBuildNM1(template1,template2,template3) {
+    return function(gottenDictionariess) {
+        var htmls = gBuildNMN(template1,template2)(gottenDictionariess);
+        return fulfillHtml(template3)({ "data": nodeToString(fold(htmls)) });
+    };
+}
+
+function nodeToString(item) {
+    var tmp = document.createElement("div");
+    tmp.appendChild(item.getElementsByTagName("body")[0].firstChild);
+    return tmp.innerHTML;
+}
+
+function fulfillHtml(template) {
+    return (data) => { return htmlFromString(fulfillTemplate(template, data)); }
+}
+
+function fulfillTemplate(template, data) {
+    var tmp = template;
+    var toFulfill = tmp.match(/{{(.*?)}}/g);
+    toFulfill.forEach(function (e, i, a) {
+        tmp = tmp.replace(e, data[e.slice(2, e.length - 2)]);
+    });
+    return tmp;
+}
+
+function fold(htmls) {
+    return htmls.reduce(
+        (total, current) => {
+            total.getElementsByTagName("body")[0].firstChild.appendChild(
+                current.getElementsByTagName("body")[0].firstChild);
+            return total; },
+        (new DOMParser()).parseFromString("<div></div>", "text/html"));
+}
+
+function htmlFromString(string) {
+    return (new DOMParser()).parseFromString(string, "text/html");
+}
+
+// injection functions
+
+function gInjectN(nodeGetter, injection) {
     return function(builtElements) {
         builtElements.forEach(b => {
-            injection(nodeGetter(), b)
+            injection(nodeGetter(), b.getElementsByTagName("body")[0].firstChild);
         });
-    }
+    };
 }
 
-function gInjectNto1(nodeGetter, injection) {
+function gInject1(nodeGetter, injection) {
     return function(builtElement) {
-        injection(nodeGetter(), builtElement);
-    }
+        injection(nodeGetter(), builtElement.getElementsByTagName("body")[0].firstChild);
+    };
 }
